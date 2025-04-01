79000787a269f2970e8ba78b9a4c795a7f64404c
Karin <abramowitzkarin@gmail.com>
2025-04-01 14:46:48 +0300

    now using ListView

--- app/src/main/java/com/example/contactsapp18032025/MainActivity.java
+++ app/src/main/java/com/example/contactsapp18032025/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.contactsapp18032025;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,12 +16,15 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et1, et2, et3, et4;
-    RecyclerView rc;
+    ArrayList<String> arrList;
+    ArrayAdapter<String> arrAdapt;
+    ListView lv;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +44,15 @@ public class MainActivity extends AppCompatActivity {
         et3 = findViewById(R.id.et3);
         et4 = findViewById(R.id.et4);
         btn = findViewById(R.id.button);
-        rc = findViewById(R.id.rv);
-
-        rc.setLayoutManager(new LinearLayoutManager(this));
-        List<Notes> noteList = noteDao.getAllNotes();
-        ContactAdapter adapter = new ContactAdapter(this);
-        rc.setAdapter(adapter);
+        lv = findViewById(R.id.listView);
+        arrList = new ArrayList<>();
+        List<Notes> list;
+        list = noteDao.getAllNotes();
+        for(Notes n: list){
+            arrList.add(String.valueOf(n.getId()));
+        }
+        arrAdapt = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrList);
+        lv.setAdapter(arrAdapt);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,14 +68,13 @@ public class MainActivity extends AppCompatActivity {
                 else{
                     Notes temp = new Notes(name, phone, email, address);
                     noteDao.insert(temp);
-                    adapter.updateData(noteDao.getAllNotes());et1.setText("");
+                    et1.setText("");
                     et2.setText("");
                     et3.setText("");
                     et4.setText("");
 
                     Toast.makeText(MainActivity.this, "Contact added!", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
     }
